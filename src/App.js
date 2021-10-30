a443fba915fc9097434aca439aff7f36b9028543
BrandGerry <gerry.marquet@gmail.com>
2021-10-30 12:44:55 -0500

    Nuevo

--- src/App.js
+++ src/App.js
@@ -3,50 +3,38 @@ import "./App.css";
 
 //Components
 import Header from "./components/Header";
-import Todo from "./components/Todo";
-
+import Loader from "./components/Loader";
 
 const App = () => {
   //STATE
-  const [dataAPI, setDataAPI] = useState([]);
+  const [todolist, setTodoList] = useState([])
+  
 
   //EFFECT
   useEffect(() =>{
-    const handleDataApi = async () =>{
+    const handleTodoList = async () =>{
       const response = await fetch("https://jsonplaceholder.typicode.com/todos");
       const result = await response.json();
-      const todos = result.slice(0,20)
-      setDataAPI(todos)
+      const resultTodoList = result.slice(0,20);
+      setTodoList(resultTodoList); //Setear el todolist
     }
-    handleDataApi();
+    setTimeout(() =>{
+      handleTodoList();},3000);
   },[]);
  
   //FUNCIONES
   //Tenemos que ver a cual le doy click con el ID y el status Completed o incompleted
-  
-  const handleTodo = (id) =>{
-    //alert("Si jaladeras"); Nada mas para ver si jala
-    //Para obtener todos los arreglos menos el qie nos piden spread
-    setDataAPI(dataAPI.map(todo => todo.id === id ? {...todo , completed : !todo.completed} : todo))
-
-  }
  
   return (
     <div className="App">
       <Header/>
       {
-        //Hago un mapeo de setdatapi que contiene los valores de la api
-        //Medainte props se las mandaremos al todo
-        dataAPI.map(todo =>(
-          <Todo 
-            title={todo.title} 
-            status={todo.completed} 
-            handleTodo={handleTodo}
-            id={todo.id}
-          />
-        ))
+        todolist ?todolist.map(singleTodo =>( //Siempre decir si todolist existe haz esto
+          <>
+          <h1>{singleTodo.title}</h1>
+          </>
+        )) : <Loader/>
       }
-
     </div>
   );
 };
---
+++ src/components/Loader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//STYLES
+
+
+
+const Loader = () => {
+  return (
+      <div className="loader">
+          <p>Cargando...</p>
+      </div>
+  );
+};
+
+export default Loader;
